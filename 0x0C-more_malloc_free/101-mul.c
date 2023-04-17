0be47835dc6afaf65e3776bbdad6da6d4ea41f11
SlimjimmyMax <slimjimmymax@outlook.com>
2023-04-17 08:26:21 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,52 +1,103 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
+#define ERR_MSG "Error"
 
 /**
- * _realloc - reallocates a memory block using malloc and free
- * @ptr: pointer to previously allocated memory
- * @old_size: byte size of allocated space for ptr
- * @new_size: byte size of newly allocated space
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: Null if new_size == 0 , ptr is not null.
- *          Ptr if new_size == old_size
- *         Pointer to newly allocated memory block
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
 
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+int is_digit(char *s)
 {
-	char *p;
-	unsigned int i;
-	unsigned int limit;
-	char *ptr_copy;
+	int i = 0;
 
-	if (ptr == NULL)
+	while (s[i])
 	{
-		p = malloc(new_size);
-		return (p);
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
+	return (1);
+}
 
-	else if (new_size == 0)
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		free(ptr);
-		return (NULL);
+		i++;
 	}
+	return (i);
+}
 
-	else if (new_size == old_size)
-		return (ptr);
+/**
+ * errors - handles errors for main
+ */
 
-	p = malloc(new_size);
-	if (p == NULL)
-		return (NULL);
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
 
-	limit = new_size;
-	if (new_size > old_size)
-		limit = old_size;
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
+ */
 
-	ptr_copy = ptr;
-	for (i = 0; i < limit; i++)
-		p[i] = ptr_copy[i];
+int main(int argc, char *argv[])
 
-	free(ptr);
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	return (p);
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
