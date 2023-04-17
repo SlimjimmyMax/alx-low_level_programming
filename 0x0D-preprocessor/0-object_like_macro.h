bf6fee519ef0c003d971c0c8b22302095d50ec84
SlimjimmyMax <slimjimmymax@outlook.com>
2023-04-17 07:13:46 -0700

    0. Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,7 @@
+/**
+
+SIZE - Macro that defines the value of size as 1024
+*/
+#ifndef SIZE
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Design to introduce the use of macros and include guards in C programming.
