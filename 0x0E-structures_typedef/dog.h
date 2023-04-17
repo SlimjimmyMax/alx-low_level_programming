9b50740ca8e68ba564b5a8d442197457448c1db7
SlimjimmyMax <slimjimmymax@outlook.com>
2023-04-17 06:31:13 -0700

    3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef FILE_DOG
+#define FILE_DOG
 
 /**
- * dog_t - Typedef for struct dog
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
  */
-typedef struct dog dog_t;
 
-/**
- * struct dog - struct that stores some information of a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Description: struct called "dog" that stores its name, its age
- * and the name of its owner.
- */
 struct dog
 {
 	char *name;
@@ -22,41 +15,14 @@ struct dog
 	char *owner;
 };
 
-/**
- * init_dog - initialize a variable of type struct dog
- * @d: pointer to the struct dog to initialize
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: void
- */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-/**
- * print_dog - prints a struct dog
- * @d: pointer to the struct dog to print
- *
- * Return: void
- */
 void print_dog(struct dog *d);
 
-/**
- * new_dog - creates a new dog struct
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Return: pointer to the new dog struct, or NULL if it fails
- */
 dog_t *new_dog(char *name, float age, char *owner);
 
-/**
- * free_dog - frees the memory allocated for a dog struct
- * @d: pointer to the dog struct to free
- *
- * Return: void
- */
 void free_dog(dog_t *d);
 
 #endif
