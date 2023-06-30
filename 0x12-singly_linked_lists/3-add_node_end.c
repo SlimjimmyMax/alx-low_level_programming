a4e9d8078580cc1c93f64ca60dcdf21623e5aa92
SlimjimmyMax <slimjimmymax@outlook.com>
2023-06-30 13:09:44 +0000

    3. Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,54 +1,50 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
 
 /**
- * _strlen - Finds the length of a string.
- * @str: String to find the length of.
+ * add_node_end - Adds a new node at the end
+ *                of a list_t list.
+ * @head: A pointer the head of the list_t list.
+ * @str: The string to be added to the list_t list.
  *
- * Return: Length of the string.
- */
-unsigned int _strlen(char *str)
-{
-    unsigned int i;
-
-    for (i = 0; str[i]; i++)
-        ;
-    return (i);
-}
-
-/**
- * add_node_end - Adds a new node to the end of a linked list.
- * @head: Double pointer to the linked list.
- * @str: String to add to the new node.
- *
- * Return: Pointer to the new node.
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new, *tmp;
+	char *dup;
+	int len;
+	list_t *new, *last;
 
-    if (str == NULL)
-        return (NULL);
-    new = malloc(sizeof(list_t));
-    if (new == NULL)
-        return (NULL);
-    new->str = strdup(str);
-    if (new->str == NULL)
-    {
-        free(new);
-        return (NULL);
-    }
-    new->len = _strlen(new->str);
-    new->next = NULL;
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
-    tmp = *head;
-    while (tmp->next)
-        tmp = tmp->next;
-    tmp->next = new;
-    return (new);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	else
+	{
+		last = *head;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
+	}
+
+	return (*head);
 }
+
