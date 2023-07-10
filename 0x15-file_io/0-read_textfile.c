119f868b0bbc38bd9e1cd75797d69863b2a2597b
SlimjimmyMax <slimjimmymax@outlook.com>
2023-07-10 13:01:22 +0000

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,36 +5,38 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int file_descriptor;
-    char *buffer;
-    ssize_t bytes_read, bytes_written;
+	int file_descriptor;
+	char *buffer;
+	ssize_t bytes_read, bytes_written;
 
-    if (filename == NULL)
-        return 0;
+	if (filename == NULL)
+		return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-        return 0;
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
 
-    buffer = malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL) {
-        close(file_descriptor);
-        return 0;
-    }
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		close(file_descriptor);
+		return (0);
+	}
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1) {
-        close(file_descriptor);
-        free(buffer);
-        return 0;
-    }
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		close(file_descriptor);
+		free(buffer);
+		return (0);
+	}
 
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    close(file_descriptor);
-    free(buffer);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	close(file_descriptor);
+	free(buffer);
 
-    if (bytes_written == -1 || bytes_written != bytes_read)
-        return 0;
+	if (bytes_written == -1 || bytes_written != bytes_read)
+		return (0);
 
-    return bytes_written;
+	return (bytes_written);
 }
