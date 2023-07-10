9696215e5f76d2c1dc0278d30766513ad3dbf4f8
SlimjimmyMax <slimjimmymax@outlook.com>
2023-07-10 13:30:57 +0000

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <string.h>
 
 /**
  * create_file - Creates a file with the given filename and writes the
@@ -14,10 +15,12 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int file_descriptor;
+
 	if (filename == NULL)
 		return (-1);
 
-	int file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_descriptor == -1)
 		return (-1);
 
@@ -32,5 +35,6 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(file_descriptor);
+
 	return (1);
 }
