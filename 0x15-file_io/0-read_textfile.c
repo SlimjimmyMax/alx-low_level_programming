dca245f24146eb222ecd4c095a991a1c24f40f19
SlimjimmyMax <slimjimmymax@outlook.com>
2023-07-10 13:10:06 +0000

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,6 +3,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed.
+ *         0 if the file cannot be opened or read, or if @filename is NULL.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file_descriptor;
