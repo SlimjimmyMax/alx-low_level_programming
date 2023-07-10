dc86b940ee31d7f2419f7bb6715076d48311ce6b
SlimjimmyMax <slimjimmymax@outlook.com>
2023-07-10 13:39:52 +0000

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,10 +13,12 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int file_descriptor;
+
 	if (filename == NULL)
 		return (-1);
 
-	int file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
 	if (file_descriptor == -1)
 		return (-1);
 
